79880bbbadda3b8abfc09ce3be1659373e3b1bc7
Vasan Arul <vasankarul@gmail.com>
2020-10-15 16:37:45 -0700

    Completion of strings and variables modules

--- python_sandbox_starter/strings.py
+++ python_sandbox_starter/strings.py
@@ -1,6 +1,66 @@
 # Strings in python are surrounded by either single or double quotation marks. Let's look at string formatting and some string methods
 
+name = "Brad"
+age = 37
+
+# Concatenate
+print('Hello, my name is ' + name + ' and I am ' + str(age))
 
 # String Formatting
 
+# Arguments by position
+print('My name is {name} and I am {age}'.format(name=name, age=age))
+
+# F-Strings (3.6+)
+print(f'Hello, my name is {name} and I am {age}')
+
 # String Methods
+
+s = 'hello world'
+
+# Capitalize string
+print(s.capitalize())
+
+# Make all uppercase
+print(s.upper())
+
+# Make all lower
+print(s.lower())
+
+# Swap case
+print(s.swapcase())
+
+# Get length
+print(len(s))
+
+# Replace
+print(s.replace('world', 'everyone'))
+
+# Count
+sub ='h'
+print(s.count(sub)) # output : 1 h in hello world
+
+# Starts with
+print(s.startswith('hello'))
+
+# Ends with
+print(s.endswith('d'))
+
+# Split into a list
+print(s.split())
+
+# Find position
+print(s.find('r'))
+
+# Is all alphanumeric (spaces means its false)
+print(s.isalnum())
+
+# Is all alphabetic (spaces means its false)
+print(s.isalpha())
+
+# Is all numeric
+print(s.isnumeric())
+
+
+
+
