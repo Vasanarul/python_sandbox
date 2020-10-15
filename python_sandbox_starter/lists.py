70000c31c7d5a7997d981d9dafee546b7451c8c3
Vasan Arul <vasankarul@gmail.com>
2020-10-15 16:52:14 -0700

    Completion of Lists module

--- python_sandbox_starter/lists.py
+++ python_sandbox_starter/lists.py
@@ -2,8 +2,40 @@
 
 # Create list
 numbers = [1, 2, 3, 4, 5]
+fruits = ['Apples', 'Oranges', 'Grapes', 'Pears']
 
 # Use a constructor
 numbers2 = list((1, 2, 3, 4, 5))
+  # print(numbers, numbers2)
 
-print(numbers, numbers2)
+# Get a value
+print(fruits[1])
+
+# Get length
+print(len(fruits))
+
+# Append to list
+fruits.append("Mangos")
+
+# Remove from list
+fruits.remove("Grapes")
+
+# Insert into position
+fruits.insert(2, 'Strawberries')
+
+# Change a value
+fruits[0] = "Blueberries"
+
+# Remove with pop
+fruits.pop(2)
+
+# Reverse list
+fruits.reverse()
+
+# Sort list
+fruits.sort()
+
+# Reverse sort
+fruits.sort(reverse=True)
+
+print(fruits)
